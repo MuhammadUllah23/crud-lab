42c4539e8c2f9812c936be446ecf3a9bc5aecef8
Muhammad <muhammadhullah23@gmail.com>
2021-11-27 20:41:17 -0500

    commit tree 5ba7e1bd0616c8805baf1ec041f0677d54c21612

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,18 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const restReview = reviews.filter(review => review.restaurantId === restaurantId)
+
+    const reviewList = restReview.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={deleteReview} />
+    })
+
     return (
       <ul>
         Reviews
+        {reviewList}
       </ul>
     );
   }
